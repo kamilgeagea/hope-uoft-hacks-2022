7033375c3b7e5106603d5fd5fa2b18ddd68eafbf
kamilgeagea <kamilgeagea8199@gmail.com>
2022-02-06 08:15:10 -0500

    fixed gradle and login screen

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const CupertinoApp(
-      home: Posts(),
+      home: Launch(),
       debugShowCheckedModeBanner: false,
       title: 'Launch',
       theme: CupertinoThemeData(
